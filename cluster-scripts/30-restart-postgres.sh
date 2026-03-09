69c8d5e52d30027ffeedbdcfd97ec6c4661ba9e5
mkc188 <mkc188@gmail.com>
2026-03-09 00:11:34 +0000

    Fix again

--- cluster-scripts/30-restart-postgres.sh
+++ cluster-scripts/30-restart-postgres.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -Eeo pipefail
 echo "Restarting PostgreSQL to apply configuration changes..."
-if command -v gosu >/dev/null 2>&1; then
-  gosu postgres pg_ctl -D "$PGDATA" -m fast restart
-else
-  su - postgres -c "pg_ctl -D \"$PGDATA\" -m fast restart"
-fi
+pg_ctl -D "$PGDATA" -m fast restart
